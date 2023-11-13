1e5b4fb9e889a863b8697b005b32b12799593a3d
Víctor Manuel Chaves García <victormcg96@gmail.com>
2023-11-13 09:00:04 +0100

    Add replace the function call for a function declaration

--- pw-2-art.js
+++ pw-2-art.js
@@ -6,31 +6,40 @@ module.exports = function PwToArt({types: t}) {
           path.remove();
         }
       },
-      CallExpression(path) {
-        if(path.node.callee.name !== 'test') return;
-        if(!t.isIdentifier(path.node.callee)) return;
-
+      ExpressionStatement(path) {
+        const expressionChild = path.node.expression;
+        if(!t.isCallExpression(expressionChild)) return;
+        if(expressionChild.callee.name !== 'test') return;
         const functionName = getFunctionName(path);
-        const executingBlock = path.node.arguments[1].body;
+        const executingBlock = expressionChild.arguments[1].body;
+        const params = expressionChild.arguments[1].params;
         const functionDeclaration = t.functionDeclaration(
           t.identifier(functionName), 
-          [],
-          executingBlock
+          params,
+          t.blockStatement([executingBlock]),
+          false,
+          true
         );
-
         path.replaceWith(functionDeclaration);
+      },
+      CallExpression(path) {
+        if(t.isMemberExpression(path.node.callee)) console.log(path.node.callee.property.name)
+        // check if name is expect and replace by null:
+        if(t.isMemberExpression(path.node.callee) && path.node.callee.property.name === 'expect') {
+          
+          path.remove();
+        }
       }
     }
   };
 };
 
 
-const getFunctionName = (path)=> {
-
-  const parent = path.parent;
-  const fn = parent && parent.leadingComments?.[0]?.value.trim();
+const getFunctionName = (parent_path)=> {
+  const parent_node = parent_path.node;
+  const fn = parent_node && parent_node.leadingComments?.[0]?.value.trim();
   
-  const altNameFunction = `execute_${path.parentPath.parent.body.indexOf(path.parent)}`;
+  const altNameFunction = `execute_${parent_path.parent.body.indexOf(parent_node)}`;
   if (!fn) return altNameFunction;
 
   return fn.replace(/@pw2art: /, '');
