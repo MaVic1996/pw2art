ac4641015a24f2108410f38d5ac4fb8861a34f7b
Víctor Manuel Chaves García <vmchaves@tirant.com>
2023-11-15 16:07:35 +0100

    Add logic to handle hooks and describe methods, also add the export sentence

--- pw-2-art.js
+++ pw-2-art.js
@@ -6,41 +6,100 @@ module.exports = function PwToArt({types: t}) {
           path.remove();
         }
       },
-      ExpressionStatement(path) {
+      ExpressionStatement(path, state) {
+        if(checkIsExpectSentence(path)) 
+          return path.remove();
+
+        if(checkIsTestDescribe(path, t))
+          return extractBodyFromTestDescribe(path);
+
+        if(checkIsTestHook(path, t))
+          return path.remove();
+        
+        if(path.node.expression?.callee?.name === 'appScenario') 
+          return path.remove();
+
         const expressionChild = path.node.expression;
         if(!t.isCallExpression(expressionChild)) return;
         if(expressionChild.callee.name !== 'test') return;
-        const functionName = getFunctionName(path);
+        const functionName = getFunctionName(path, state);
         const executingBlock = expressionChild.arguments[1].body;
-        const params = expressionChild.arguments[1].params;
+        const params = expressionChild.arguments[1].params?.[0];
+        const { properties } = params;
+        const paramName = t.isIdentifier(properties[0].key) ? properties[0].key.name : null;
+        state.generatedFunctions.push(functionName);
+
         const functionDeclaration = t.functionDeclaration(
           t.identifier(functionName), 
-          params,
+          [t.identifier(paramName)],
           t.blockStatement([executingBlock]),
           false,
           true
         );
         path.replaceWith(functionDeclaration);
       },
-      CallExpression(path) {
-        if(t.isMemberExpression(path.node.callee)) console.log(path.node.callee.property.name)
-        // check if name is expect and replace by null:
-        if(t.isMemberExpression(path.node.callee) && path.node.callee.property.name === 'expect') {
-          
-          path.remove();
-        }
+      Program: {
+        enter(_, state) {
+          // Inicializa el array en el estado
+          state.generatedFunctions = [];
+        },
+        exit(path, state) {
+          // Al finalizar el análisis del programa, inserta exportaciones al final del código
+          const specifiers = state.generatedFunctions.map((funcName) => {
+            const id = t.identifier(funcName);
+            return  t.objectProperty(id, id);
+          });
+
+          const objectExpression = t.objectExpression(specifiers);
+
+          const moduleExports = t.expressionStatement(
+            t.assignmentExpression(
+              '=',
+              t.memberExpression(t.identifier('module'), t.identifier('exports')),
+              objectExpression
+            )
+          );
+
+
+
+          path.pushContainer('body', moduleExports);
+        },
       }
     }
   };
 };
 
 
-const getFunctionName = (parent_path)=> {
+const getFunctionName = (parent_path, state)=> {
   const parent_node = parent_path.node;
   const fn = parent_node && parent_node.leadingComments?.[0]?.value.trim();
   
-  const altNameFunction = `execute_${parent_path.parent.body.indexOf(parent_node)}`;
+  const altNameFunction = `execute_${state.generatedFunctions.length + 1}`;
   if (!fn) return altNameFunction;
 
   return fn.replace(/@pw2art: /, '');
+}
+
+const extractBodyFromTestDescribe = (path) => {
+  const bodyNodes = path.node.expression.arguments[1].body.body;
+  path.replaceWithMultiple(bodyNodes);
+}
+
+const checkIsExpectSentence = (path) => {
+  return path?.node?.expression?.argument?.callee?.object?.callee?.name === 'expect';
+};
+
+const checkIsTestDescribe = (path, t) => {
+  return t.isCallExpression(path.node.expression)  &&
+    path.node.expression.callee?.object?.name === 'test' &&
+    path.node.expression.callee?.property?.name === 'describe';
+}
+
+const checkIsTestHook = (path, t) => {
+  if (!t.isCallExpression(path.node.expression)  ||
+    path.node.expression.callee?.object?.name !== 'test')
+    return false;
+  const calleeProp = path.node.expression.callee?.property?.name;
+
+  return !!calleeProp  && ['before', 'after'].filter((part_hook) => calleeProp.includes(part_hook)).length > 0;
 }
